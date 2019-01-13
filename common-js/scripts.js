42c7c73e728c23ec942541652589821cd47e247a
Glitch (hello-webpage) <none>
2019-01-13 04:25:40 +0000

    🐯☠️ Checkpoint ./index.html:8/26 ./common-js/scripts.js:8/212

---
+++ common-js/scripts.js
@@ -0,0 +1,9 @@
+// Update emoji title based on time of day:
+$(function(){
+  const hours = (new Date).getHours();
+  //night: 0-4 or 20-24
+  //dawn: 4-8
+  //day: 8-16
+  //dusk: 16-20
+  //<!-- 🏙️🌆🌃🌇 /-->
+});
