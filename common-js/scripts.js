4c0cc4ba8808a7bac589dc01b36d6e44bfe268e0
Glitch (hello-webpage) <none>
2019-01-13 04:35:44 +0000

    🌥🍨 Checkpoint ./common-js/scripts.js:8/447 ./index.html:8/8

--- common-js/scripts.js
+++ common-js/scripts.js
@@ -1,9 +1,19 @@
 // Update emoji title based on time of day:
 $(function(){
   const hours = (new Date).getHours();
+  //🏙️🌆🌃🌇
   //night: 0-4 or 20-24
   //dawn: 4-8
   //day: 8-16
   //dusk: 16-20
-  //<!-- 🏙️🌆🌃🌇 /-->
+  let emoji = "🌃";
+  if(hours >= 4 && hours < 8) {
+    emoji = "🌆";
+  } else if (hours >= 8 && hours < 16){
+    emoji = "🏙️";
+  } else if (hours >= 16 && hours < 20) {
+    emoji = "🌇"
+  }
+  
+  document.title = emoji + " Allred.nyc";
 });
