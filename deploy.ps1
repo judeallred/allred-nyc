509304c452a3ef909f886064360c4326b9a4514f
Jude Allred <j@hash.ai>
2020-09-14 16:37:48 -0400

    Remove glitch cruft; server.js, etc.

--- deploy.ps1
+++ deploy.ps1
@@ -1,1 +1,3 @@
+Get-Date > www/last-deploy.txt
 aws s3 sync www s3://www.allred.nyc --delete --profile allred-nyc
+rm www/last-deploy.txt
--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.use(express.static("www"));
-
-const listener = app.listen(process.env.PORT, () => {
-  console.log("Your app is listening on port " + listener.address().port);
-});
